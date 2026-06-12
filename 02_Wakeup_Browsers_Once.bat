b584ada95c97ff6210a0e82814541a0ce44a6880
ㅁㅁ <53392118+localread@users.noreply.github.com>
2026-06-12 23:55:06 +0900

    Delete 02_Wakeup_Browsers_Once.bat

--- 02_Wakeup_Browsers_Once.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-title LocalReadLog Browser Wakeup Once
-set "LRL_ROOT=%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0core\localreadlog_browser_wakeup_once.ps1" -Root "%LRL_ROOT%" -WaitSeconds 12 -VerboseLog
-pause
